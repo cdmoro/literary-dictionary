0a51d6f8f95bcc31853e6f898c5389df4b3afee8
Carlos Bonadeo <carlosbonadeo@gmail.com>
2025-06-08 18:59:17 +0100

    v1.0.0

--- src/pages/toc.py
+++ src/pages/toc.py
@@ -25,17 +25,17 @@ def get_toc_page(lang, strings, pages_by_section):
 
         template += f'  <div><a href="{section}/{section}.xhtml">{strings[section.lower()]}</a></div>\n'
 
-        template += '  <div class="toc-letters">\n'
+        template += '  <div class="toc-letters">'
 
         letter_links = [file for file in files if re.fullmatch(r"[A-Z]_[A-Z]", file)]
 
         for index, file in enumerate(letter_links):
-            if index % 10 == 0 and index != 0:
-                template += "    <br/>\n"
+            if index % 13 == 0 and index != 0:
+                template += "<br/>"
 
-            template += f'    <a class="toc-letter" href="{section}/{file}.xhtml">{file.split("_")[1]}</a>\n'
+            template += f'<a class="toc-letter" href="{section}/{file}.xhtml">{file.split("_")[1]}</a>&nbsp;&nbsp;&nbsp;'
 
-        template += "  </div>\n"
+        template += "</div>\n"
 
     template += """</body>
 </html>
