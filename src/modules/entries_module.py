df5667a5c11d4af410503f47f4668703820844bb
Carlos Bonadeo <carlosbonadeo@gmail.com>
2025-06-19 21:01:52 +0100

    Fix markup & add new translations

--- src/modules/entries_module.py
+++ src/modules/entries_module.py
@@ -20,9 +20,9 @@ def get_entry_markup(
         template += f"""    <idx:entry name="default" scriptable="yes" spell="yes" id="{id}">
       <idx:short>\n"""
     else:
-        template += "<div>"
+        template += "    <div>\n"
 
-    template += f'    <a id="{id}"></a>'
+    template += f'      <a id="{id}"></a>\n'
 
     if dict_markup:
         template += f"""<idx:orth value="{html.escape(name)}"><strong>{escape_text_nodes(display_name)}</strong></idx:orth>\n"""
@@ -32,36 +32,35 @@ def get_entry_markup(
                 template += f'        <idx:orth value="{html.escape(alias)}" />\n'
         template += "\n"
     else:
-        template += f"<strong>{html.escape(display_name)}</strong>"
-        template += "</div>\n"
+        template += f"      <strong>{html.escape(display_name)}</strong>\n"
+        template += "    </div>\n\n"
 
     # Definition
-    template += "      \n"
-    template += "<div class='definition'>"
+    template += '    <div class="definition">\n'
 
-    template += f"""        <div>{f"<em>{html.escape(abbr)}.</em> " if abbr else ""}{escape_text_nodes(description)}</div>"""
+    template += f"""      <div>{f"<em>{html.escape(abbr)}.</em> " if abbr else ""}{escape_text_nodes(description)}</div>"""
 
     for title, desc in additional_info.items():
         if len(desc) > 0:
-            template += f"""\n        <div>
+            template += f"""\n      <div>
           <strong>{title}:</strong> {desc}
-        </div>"""
+      </div>\n"""
 
-    template += "</div>"
+    template += "    </div>\n"
 
     if dict_markup:
         template += "</idx:short>\n"
 
     if additional_info_extended:
-        template += '<div class="extended-definition">'
+        template += '    <div class="extended-definition">'
 
         for title, desc in additional_info_extended.items():
             if len(desc) > 0:
-                template += f"""\n        <div>
+                template += f"""\n          <div>
             <strong>{title}:</strong> {desc}
             </div>"""
 
-        template += "</div>"
+        template += "</div>\n"
 
     if dict_markup:
         template += "  </idx:entry>\n\n"
